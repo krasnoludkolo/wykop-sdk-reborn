5f6686f97cdff6f81d06d696592f0c2e8e5a4754
Danek <janek@projmen.pl>
2020-11-17 13:54:33 +0100

    Remove v1

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 
-from wykop.api.v1.clients import WykopAPIv1
 from wykop.api.v2.clients import WykopAPIv2
 from wykop.api.requesters.requests import RequestsRequester
 from wykop.api.requesters.urllib import UrllibRequester
@@ -14,15 +13,6 @@ def requests_requester():
 @pytest.fixture
 def urllib_requester():
     return UrllibRequester()
-
-
-@pytest.fixture
-def wykop_api_v1():
-    appkey = '123456app'
-    secretkey = '654321secret'
-    api = WykopAPIv1(appkey, secretkey)
-    api._domain = 'api.test.com'
-    return api
 
 
 @pytest.fixture
