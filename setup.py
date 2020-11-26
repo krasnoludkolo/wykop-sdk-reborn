a469fed905e7a9c056ca5be5b0658061ba64102a
janek@projmen.pl <janek@projmen.pl>
2020-11-26 18:45:16 +0100

    Update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 with open('requirements_test.txt') as f:
     tests_requires = f.read().splitlines()
 
-version = '0.1.6'
+version = '0.1.7'
 
 setup(
     name='wykop-sdk-reborn',
