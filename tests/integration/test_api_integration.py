401c0fcaa014683dd897d914f9f09e8d4b4c25d3
Danek <janek@projmen.pl>
2020-12-05 19:32:19 +0100

    Add integration test related to account

--- tests/integration/test_api_integration.py
+++ tests/integration/test_api_integration.py
@@ -2,15 +2,27 @@ import os
 from wykop import WykopAPI
 
 
+def create_client():
+    key = os.environ.get('WYKOP_TAKTYK_KEY')
+    secret = os.environ.get('WYKOP_TAKTYK_SECRET')
+    account_key = os.environ.get('WYKOP_TAKTYK_ACCOUNTKEY')
+    return WykopAPI(key, secret, output='clear', account_key=account_key)
+
+
 class TestApiIntegration(object):
 
     def test_api_connection(self):
-        key = os.environ.get('WYKOP_TAKTYK_KEY')
-        secret = os.environ.get('WYKOP_TAKTYK_SECRET')
-        account_key = os.environ.get('WYKOP_TAKTYK_ACCOUNTKEY')
-        api = WykopAPI(key, secret, output='clear')
+        api = create_client()
 
-        api.authenticate(account_key=account_key)
+        api.authenticate()
         entries = api.entries_hot()
 
         assert isinstance(entries, dict)
+
+    def test_api_connection_with_method_related_to_account(self):
+        api = create_client()
+
+        api.authenticate()
+        conversations = api.conversations_list()
+
+        assert isinstance(conversations, dict)
