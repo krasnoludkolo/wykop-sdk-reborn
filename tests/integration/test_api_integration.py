42bffd82628da6ef471f643c1a4240117c95d5cb
Danek <janek@projmen.pl>
2021-01-09 12:11:19 +0100

    Use all api clients in integration tests

--- tests/integration/test_api_integration.py
+++ tests/integration/test_api_integration.py
@@ -1,28 +1,37 @@
 import os
-from wykop import WykopAPI
+from wykop import WykopAPI, MultiKeyWykopAPI
 
 
-def create_client():
+def create_base_client():
     key = os.environ.get('WYKOP_TAKTYK_KEY')
     secret = os.environ.get('WYKOP_TAKTYK_SECRET')
     account_key = os.environ.get('WYKOP_TAKTYK_ACCOUNTKEY')
     return WykopAPI(key, secret, output='clear', account_key=account_key)
 
 
+def create_multi_key_client():
+    key = os.environ.get('WYKOP_TAKTYK_KEY')
+    secret = os.environ.get('WYKOP_TAKTYK_SECRET')
+    account_key = os.environ.get('WYKOP_TAKTYK_ACCOUNTKEY')
+    return MultiKeyWykopAPI([(key, secret, account_key)], output='clear')
+
+
+def all_api_clients():
+    return [create_base_client(), create_multi_key_client()]
+
+
 class TestApiIntegration(object):
 
-    def test_api_connection(self):
-        api = create_client()
+    def test_all_api_clients_connection(self):
+        for api in all_api_clients():
+            entries = api.entries_hot()
 
-        api.authenticate()
-        entries = api.entries_hot()
+            assert isinstance(entries, list)
 
-        assert isinstance(entries, list)
+    def test_all_api_clients_connection_with_method_related_to_account(self):
+        for api in all_api_clients():
 
-    def test_api_connection_with_method_related_to_account(self):
-        api = create_client()
+            api.authenticate()
+            conversations = api.conversations_list()
 
-        api.authenticate()
-        conversations = api.conversations_list()
-
-        assert isinstance(conversations, list)
+            assert isinstance(conversations, list)
